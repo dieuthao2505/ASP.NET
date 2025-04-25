a84febafe96428053f7c3f92aa0f60bd8caa8c3f
dieuthao2505 <dieuthao2505@gmail.com>
2025-04-25 14:42:43 +0700

    Initial commit

---
+++ thao/thao/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using thao.Data;
+
+namespace thao.Controllers
+{
+    public class ProductController : Controller
+    {
+        QLIKHACHHANGEntities1 objQLIKHACHHANGEntities1 = new QLIKHACHHANGEntities1();
+        public ActionResult Index()
+        {
+            var lstProduct = objQLIKHACHHANGEntities1.Products.ToList();
+            return View(lstProduct);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Product objPro)
+        {
+            objQLIKHACHHANGEntities1.Products.Add(objPro);
+            //objQLIKHACHHANGEntities1.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ thao/thao/Controllers/QLIKHACHHANGEntities1.cs
@@ -0,0 +1,10 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using thao.Data;
+
+//namespace thao.Controllers
+//{
+
+//}
